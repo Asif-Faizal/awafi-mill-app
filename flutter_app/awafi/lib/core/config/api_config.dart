1338fbb7a454ff18fad3c599a853dbd4549e911a
Asif-Faizal <moh.asif@protonmail.ch>
2025-05-08 00:11:13 +0530

    REMOVED STRIPE KEY

--- flutter_app/awafi/lib/core/config/api_config.dart
+++ flutter_app/awafi/lib/core/config/api_config.dart
@@ -1,6 +1,9 @@
 class ApiConfig {
   static final String baseUrl = 'https://app.awafimill.com/api';
   // static final String baseUrl = 'https://testfro.adilc0070.site/api';
-  static final String stripePublishableKey = "";
-  static final String stripeSecretKey = "";
+  
+  // Stripe keys should be loaded from environment variables or secure storage
+  // DO NOT commit actual keys to the repository
+  static final String stripePublishableKey = String.fromEnvironment('STRIPE_PUBLISHABLE_KEY', defaultValue: '');
+  static final String stripeSecretKey = String.fromEnvironment('STRIPE_SECRET_KEY', defaultValue: '');
 }
